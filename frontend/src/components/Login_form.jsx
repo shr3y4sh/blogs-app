c30cfd86c175993de929e07b0269570df0d1a42b
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-04-23 22:53:36 +0530

    uibuild

--- frontend/src/components/Login_form.jsx
+++ frontend/src/components/Login_form.jsx
@@ -4,21 +4,34 @@ import React from 'react';
 import { TextField, Button, Typography } from '@mui/material';
 
 import { useUserLogin } from '../contexts/user-reducer';
+import { useSetNotifications } from '../contexts/notification-reducer';
 
 const Login_Form = () => {
 	const user = useUserLogin();
-
+	const setNotification = useSetNotifications();
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 
 	async function handleLogin(e) {
 		e.preventDefault();
+		if (username.length === 0 || password.length === 0) {
+			setNotification('Fields cannot be empty', 'error');
+			return;
+		}
 		await user.loginUser({ username, password });
 	}
 	return (
 		<>
-			<Typography variant='h4'>Login to post blogs</Typography>
-			<form onSubmit={handleLogin} className='blog-form'>
+			<form
+				onSubmit={handleLogin}
+				className='blog-form'
+				style={{
+					maxWidth: '20rem',
+					margin: 'auto'
+				}}>
+				<Typography variant='h4' style={{ margin: '1rem 0' }}>
+					Login to post blogs
+				</Typography>
 				<div className='form-controls'>
 					<TextField
 						variant='outlined'
@@ -37,7 +50,10 @@ const Login_Form = () => {
 					/>
 				</div>
 				<div>
-					<Button type='submit' variant='contained'>
+					<Button
+						type='submit'
+						variant='contained'
+						style={{ margin: '1rem 0' }}>
 						Login
 					</Button>
 				</div>
--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -4,23 +4,43 @@ import { TextField, Button, Typography } from '@mui/material';
 
 import { useUserLogin } from '../contexts/user-reducer';
 import { signUp } from '../services/users';
+import { useSetNotifications } from '../contexts/notification-reducer';
 
 const SignupForm = () => {
 	const [username, setusername] = useState('');
 	const [name, setFullName] = useState('');
 	const [password, setPassword] = useState('');
+	const setNotification = useSetNotifications();
 
 	const user = useUserLogin();
 
 	async function handleFormSubmit(e) {
 		e.preventDefault();
+		if (username === '' || password === '' || name === '') {
+			setNotification('Fields cannot be empty', 'error');
+			return;
+		}
+
+		if (password.length < 6) {
+			setNotification('Password must be atleast six characters', 'error');
+			return;
+		}
+
 		await signUp({ username, name, password });
 		await user.loginUser({ username, password });
 	}
 
 	return (
-		<form onSubmit={handleFormSubmit}>
-			<Typography variant='h3'>Sign Up</Typography>
+		<form
+			onSubmit={handleFormSubmit}
+			style={{
+				maxWidth: '20rem',
+				margin: 'auto',
+				marginBottom: '1rem'
+			}}>
+			<Typography variant='h3' style={{ margin: '1rem 0' }}>
+				Sign Up
+			</Typography>
 			<div>
 				<TextField
 					label='username'
@@ -47,7 +67,10 @@ const SignupForm = () => {
 			</div>
 
 			<div>
-				<Button type='submit' variant='contained'>
+				<Button
+					type='submit'
+					variant='contained'
+					style={{ margin: '1rem 0' }}>
 					Sign Up
 				</Button>
 			</div>
